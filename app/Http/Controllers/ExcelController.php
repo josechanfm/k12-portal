937911ca2bc72c8780f0e645e6358617fa650a1f
josechanfm <josechan@ipm.edu.mo>
2024-03-21 19:31:09 +0800

    b4for content demo

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -18,46 +18,45 @@ class ExcelController extends Controller
     public function readFile(){
         //Vendor/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Writer/Pdf/Mpdf line 43
         //$config = ['tempDir' => $this->tempDir . '/mpdf', 'mode'=>'+aCJK','autoScriptToLang'=>true,'autoLangToFont'=>true];
-
+        //$config = ['mode'=>'+aCJK','autoScriptToLang'=>true,'autoLangToFont'=>true];
+        $file=storage_path('template//CK_B4_StudentRegistrationForm.xlsx');
+        //$file=storage_path('template//ExcelSheetDemo.xlsx');
         $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader("Xlsx");
-        $spreadsheet = $reader->load(storage_path('template//ExcelSheetDemo.xlsx'));
-        $spreadsheet->getDefaultStyle()->getFont()->setName('DejaVuSans'); // 字体
-        // $spreadsheet->getDefaultStyle()->getFont()->setSize(8); // 字体大小
+        $spreadsheet = $reader->load($file);
 
-        // $styleArray=array(
-        //     'font'=>array(
-        //         'bold'  => true,
-        //         'color' => array('rgb' => 'FF0000'),
-        //         'size'  => 15,
-        //         'name'=>'DejaVuSans'
-        //     )
-        // );
-        // $spreadsheet->getDefaultStyle()->applyFromArray($styleArray);
-
-        $sheet0=$spreadsheet->getSheet(0);
         $sheet1=$spreadsheet->getSheet(1);
-        $sheet2=$spreadsheet->getSheet(2);
-        $sheet0->getCell('B3')->setValue('欧阳克');
-        $sheet1->getCell("D4")->setValue('a2 on sheet 1');
-        $sheet2->getCell("A2")->setValue('a2 on sheet 2');
-        echo  $sheet0->getCell("D4")->getValue();
-        echo  $sheet1->getCell("A1")->getValue();
-        echo  $sheet2->getCell("A1")->getValue();
-        echo '<br>';
+        $sheet1->getCell("B1")->setValue('2024');
+        $sheet1->getCell("B2")->setValue('初一');
+        $sheet1->getCell("B3")->setValue('A');
+        $sheet1->getCell("B4")->setValue('13');
+        $sheet1->getCell("B5")->setValue('陳輝民');
+        $sheet1->getCell("B6")->setValue('Jose Chan');
+        $sheet1->getCell("B7")->setValue('M');
+        $sheet1->getCell("B8")->setValue('1970');
+        $sheet1->getCell("C8")->setValue('07');
+        $sheet1->getCell("D8")->setValue('18');
+        $sheet1->getCell("B9")->setValue('中國');
 
+        $sheet1->getCell("B22")->setValue('陳大文');
+        $sheet1->getCell("C22")->setValue('父親');
+        $sheet1->getCell("D22")->setValue('54');
+        $sheet1->getCell("E22")->setValue('programmer');
+        $sheet1->getCell("F22")->setValue('my address ....');
+        $sheet1->getCell("G22")->setValue('63860836');
+
+        $sheet1->getCell("B23")->setValue('');
+        $sheet1->getCell("C23")->setValue('');
+        $sheet1->getCell("D23")->setValue('');
+        $sheet1->getCell("E23")->setValue('');
+        $sheet1->getCell("F23")->setValue('');
+        $sheet1->getCell("G23")->setValue('');
+
+        $sheet1->getCell("B28")->setValue('YES');
+        $sheet1->getCell("B32")->setValue('2024-03-20');
 
         //$spreadsheet->setActiveSheetIndex(2);
-        $locale='zh_Tw';
-        $validLocale = \PhpOffice\PhpSpreadsheet\Settings::setLocale($locale);
-        if (!$validLocale) {
-            echo 'Unable to set locale to ' . $locale . " - reverting to en_us" . PHP_EOL;
-        }
-        echo '<br>';
-        echo \PhpOffice\PhpSpreadsheet\Settings::getLocale();
-        echo '<br>';
-        $config = ['mode'=>'+aCJK','autoScriptToLang'=>true,'autoLangToFont'=>true];
+        
         $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Mpdf');
-
         $writer->save(storage_path('template//abc123.xlsx'));
         $writer->save(storage_path('template//abc123.pdf'));
 
