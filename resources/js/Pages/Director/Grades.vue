cc675050086b1532967d9bd7ef096cdea5b75415
aabbq23423 <80802803+aabbq23423@users.noreply.github.com>
2024-07-29 18:15:59 +0800

    Update Grades.vue

--- resources/js/Pages/Director/Grades.vue
+++ resources/js/Pages/Director/Grades.vue
@@ -1,13 +1,34 @@
 <template>
-<AdminLayout title="年級班別" >
-
-    <a-table :dataSource="viewGrades" class="w-1/2">
+  <AdminLayout title="年級班別" :breadcrumb="breadcrumb">
+    <div class="flex flex-wrap font-bold text-sm gap-1">
+      <div class="flex bg-gray-300 rounded-lg p-1 px-2 items-center gap-1">
+        <div class="text-gray-600 font-black rounded-l-lg bg-gray-100 p-1">
+          學年代號
+        </div>
+        <div class=" ">{{ year.code }}</div>
+      </div>
+      <div class="flex bg-gray-300 rounded-lg p-1 items-center gap-1">
+        <div class="text-gray-600 font-black rounded-l-lg bg-gray-100 p-1">
+          學年全稱
+        </div>
+        <div class=" ">{{ year.title }}</div>
+      </div>
+      <div class="flex bg-gray-300 rounded-lg p-1 items-center gap-1">
+        <div class="text-gray-600 font-black rounded-l-lg bg-gray-100 p-1">
+          日期
+        </div>
+        <div>{{ year.start }}</div>
+        <ArrowRightOutlined />
+        <div>{{ year.end }}</div>
+      </div>
+    </div>
+    <a-table :dataSource="viewGrades" class="">
         <a-table-column title="年級">
             <template #default="{record}">
                 {{ record.tag}}
             </template>
         </a-table-column>
-        <template v-for="aph in 'a'.toUpperCase().split('')" :key="aph">
+        <template v-for="aph in 'abcdefghi'.toUpperCase().split('')" :key="aph">
             <a-table-column :title="aph">
                 <template #default="{record}">
                     <div v-if="record.klasses[aph]">
@@ -23,125 +44,88 @@
         </template>
     </a-table>
 
-    <!-- <div class="">
-        <div class="">
-            <div class="">
-                <table style="table-layout: auto">
-                    <thead class="">
-                        <tr>
-                            <th class="text-left">班別學生數目</th>
-                            <th class="text-left">A</th>
-                            <th class="text-left">B</th>
-                            <th class="text-left">C</th>
-                            <th class="text-left">D</th>
-                            <th class="text-left">E</th>
-                            <th class="text-left">F</th>
-                        </tr>
-                    </thead>
-                    <tbody class="">
-                        <tr v-for="(grade, gradeKey) in grades" :grade_id="grade.id" class="ant-table-row">
-                            <td class="ant-table-cell text-left">{{ grade.tag }}</td>
-                            <template v-for="klass in grade.klasses">
-                                <td class="ant-table-cell text-left">
-                                    <template v-if="klass.grade_year <= 3">
-                                        <a-button as="link" :href="route('director.pre.klasses.show', klass.id)" class="ant-btn">{{ klass.tag }}</a-button>
-                                        {{ klass.student_count }}
-                                    </template>
-                                    <template v-else>
 
-                                        {{ klass.student_count }}
-                                    </template>
-                                </td>
-                            </template>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div> -->
-</AdminLayout>
+    <a-divider />
+  </AdminLayout>
 </template>
 
 <script>
 import AdminLayout from "@/Layouts/AdminLayout.vue";
-import {
-    TeamOutlined,
-    ArrowRightOutlined
-} from "@ant-design/icons-vue";
+import { TeamOutlined, ArrowRightOutlined } from "@ant-design/icons-vue";
 import ButtonLink from "@/Components/ButtonLink.vue";
 
 export default {
-    components: {
-        AdminLayout,
-        TeamOutlined,
-        ButtonLink,
-        ArrowRightOutlined,
-    },
-    props: ["year", "grades"],
-    data() {
-        return {
-            breadcrumb: [{
-                    label: "主控台",
-                    url: route("director.dashboard")
-                },
-                {
-                    label: "年級",
-                    url: null
-                },
-            ],
+  components: {
+    AdminLayout,
+    TeamOutlined,
+    ButtonLink,
+    ArrowRightOutlined,
+  },
+  props: ["year", "grades"],
+  data() {
+    return {
+      breadcrumb: [
+        { label: "主控台", url: route("director.dashboard") },
+        { label: "年級", url: null },
+      ],
 
-            // activeKey:'grade',
-            //courses:[],
-            //students:[],
-            // selectedKlass:{
-            //     tag:"--"
-            // }
-        };
-    },
-    mounted() {
-        // const urlParams=new URLSearchParams(window.location.search);
-        // const klassId=urlParams.get('kid');
-        // this.grades.map(grade=>{
-        //     grade.klasses.map(klass=>{
-        //         if(klass.id==klassId){
-        //             this.selectKlass(klass,'course')
-        //         }
-        //     });
-        // });
+      // activeKey:'grade',
+      //courses:[],
+      //students:[],
+      // selectedKlass:{
+      //     tag:"--"
+      // }
+    };
+  },
+  mounted() {
+    // const urlParams=new URLSearchParams(window.location.search);
+    // const klassId=urlParams.get('kid');
+    // this.grades.map(grade=>{
+    //     grade.klasses.map(klass=>{
+    //         if(klass.id==klassId){
+    //             this.selectKlass(klass,'course')
+    //         }
+    //     });
+    // });
+  },
+  methods: {
+    findKclass(record,aph){
+       return record.klasses.find(x=>x.letter==aph)??false
+    }
+    // selectKlass(klass,activeKey){
+    //     this.selectedKlass=klass;
+    //     axios.get('/director/students/get_by_klass_id/'+klass.id)
+    //         .then(response=>{
+    //             this.students = response.data;
+    //             //this.courses = [];
+    //         });
+    //     this.selectCourse(klass);
+    //     this.activeKey=activeKey;
+    //     //this.activeKey='student';
+    // },
+    // selectCourse(klass){
+    //     this.selectedKlass=klass;
+    //     axios.get('/director/courses/get_by_klass_id/'+klass.id)
+    //         .then(response=>{
+    //             this.courses = response.data;
+    //             console.log(this.courses);
+    //             //console.log(reponse.data);
+    //             //this.students = [];
+    //         });
+    // }
+  },
+  
+  computed: {
+     
+    viewGrades() {
+        let grades = JSON.parse(JSON.stringify( this.grades) );
+        grades.forEach(g => {
+            g.klasses = Object.fromEntries((g?.klasses??[]).map(k => [k.letter, k]))
+        })
+        return grades
+    }
     },
 
-    methods: {
-
-        // selectKlass(klass,activeKey){
-        //     this.selectedKlass=klass;
-        //     axios.get('/director/students/get_by_klass_id/'+klass.id)
-        //         .then(response=>{
-        //             this.students = response.data;
-        //             //this.courses = [];
-        //         });
-        //     this.selectCourse(klass);
-        //     this.activeKey=activeKey;
-        //     //this.activeKey='student';
-        // },
-        // selectCourse(klass){
-        //     this.selectedKlass=klass;
-        //     axios.get('/director/courses/get_by_klass_id/'+klass.id)
-        //         .then(response=>{
-        //             this.courses = response.data;
-        //             console.log(this.courses);
-        //             //console.log(reponse.data);
-        //             //this.students = [];
-        //         });
-        // }
-    },
-    computed: {
-        viewGrades() {
-            let grades = this.grades;
-            grades.forEach(g => {
-                g.klasses = Object.fromEntries(g.klasses.map(k => [k.letter, k]))
-            })
-            return grades
-        }
-    },
 };
 </script>
+
