778482b6ea0d6b738da927f92b664c84567bfc4b
josechanfm <josechan@ipm.edu.mo>
2024-08-02 16:02:32 +0800

    candidate, register and enroll

--- database/migrations/2024_04_02_010658_create_candidates_table.php
+++ database/migrations/2024_04_02_010658_create_candidates_table.php
@@ -39,8 +39,8 @@ return new class extends Migration
 
             $table->string('previour_school')->nullable();
             $table->string('previour_grade')->nullable();
-            $table->foreignId('start_grade')->nullable();
-            $table->foreignId('start_klass')->nullable();
+            $table->string('start_grade_tag',5)->nullable();
+            $table->string('start_klass_tag',5)->nullable();
             
             $table->string('phone')->nullable();
             $table->string('phone_home')->nullable();
