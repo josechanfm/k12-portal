8aeb4e791d1e2da7da1e1489d2d093283ed64f29
josechanfm <josechan@ipm.edu.mo>
2024-08-02 17:39:11 +0800

    topic migration

--- database/migrations/2023_04_14_092316_create_topics_table.php
+++ database/migrations/2023_04_14_092316_create_topics_table.php
@@ -17,7 +17,8 @@ return new class extends Migration
             $table->id();
             $table->foreignId('theme_id');
             $table->integer('sequence');
-            $table->string('category_code')->nullable();
+            $table->string('ability_code')->nullable();
+            $table->string('ability')->nullable();
             $table->string('section')->nullable();
             $table->string('abbr')->nullable();
             $table->string('title')->nullable();
